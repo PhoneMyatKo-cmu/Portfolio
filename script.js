7e9d1f53bb97adb26b957de3776f8567eb51f0b5
Phone <hponemyut@gmail.com>
2024-12-07 19:29:17 +0700

    responsive initial done

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+var menuFlag = false;
+console.log(document.querySelector("main"));
+function openMenu() {
+  let menu = document.querySelector(".menu");
+  if (menuFlag) {
+    console.log("in if");
+    menu.style.display = "none";
+    menuFlag = false;
+  } else {
+    console.log("in else");
+    menu.style.display = "block";
+    menuFlag = true;
+  }
+  console.log(menuFlag);
+  console.log("called.");
+}
 
+let menuIcon = document.querySelector(".hamburger-icon");
+console.log(menuIcon);
